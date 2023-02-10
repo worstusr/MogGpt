5606a2a7e35b05aa65f242fb963e572ab9f60229
worstusr <mogwaiestevan@outlook.com>
2023-02-10 14:19:19 -0300

    Update main.js

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://mog-gpt.vercel.app/', {
+  const response = await fetch('https://moggpt.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
